83ab67f6fcab374fd105e77740d8b065dfb50596
Lucas Santos <lucas.santos@otacom.com.br>
2020-08-18 19:11:31 -0300

    Repositorio Infra data

--- ApiAspNetCore/ApiAspNetCore.Infra.Data/Repositorio/UsuarioRepositorio.cs
+++ ApiAspNetCore/ApiAspNetCore.Infra.Data/Repositorio/UsuarioRepositorio.cs
@@ -1,56 +1,159 @@
 ﻿using ApiAspNetCore.Dominio.Entidades;
 using ApiAspNetCore.Dominio.Query.Usuario;
 using ApiAspNetCore.Dominio.Repositorio;
+using ApiAspNetCore.Infra.Data.Queries;
+using ApiAspNetCore.Infra.Data.Settings;
+using Dapper;
+using LSCode.ConexoesBD.DbContext;
+using LSCode.ConexoesBD.Enums;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 
 namespace ApiAspNetCore.Infra.Data.Repositorio
 {
     public class UsuarioRepositorio : IUsuarioRepositorio
     {
+        private readonly DynamicParameters _parametros = new DynamicParameters();
+        private readonly DbContext _ctx;
+        private readonly SettingsInfraData _settingsInfraData;
+
+        public UsuarioRepositorio(IOptions<SettingsInfraData> options)
+        {
+            _settingsInfraData = options.Value;
+            _ctx = new DbContext(EBancoDadosRelacional.SQLServer, _settingsInfraData.ConnectionString);
+        }
+
         public void Salvar(Usuario usuario)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _parametros.Add("Login", usuario.Login.ToString(), DbType.String);
+                _parametros.Add("Senha", usuario.Senha.ToString(), DbType.String);
+                _parametros.Add("Privilegio", usuario.Privilegio, DbType.Int16);
+
+                _ctx.SQLServerConexao.Execute(UsuarioQueries.Salvar, _parametros);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
         }
 
         public void Atualizar(Usuario usuario)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _parametros.Add("Id", usuario.Id, DbType.Int32);
+                _parametros.Add("Login", usuario.Login.ToString(), DbType.String);
+                _parametros.Add("Senha", usuario.Senha.ToString(), DbType.String);
+                _parametros.Add("Privilegio", usuario.Privilegio, DbType.Int16);
+
+                _ctx.SQLServerConexao.Execute(UsuarioQueries.Atualizar, _parametros);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
         }
 
         public void Deletar(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _parametros.Add("Id", id, DbType.Int32);
+
+                _ctx.SQLServerConexao.Execute(UsuarioQueries.Deletar, _parametros);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
         }
 
         public UsuarioQueryResult Obter(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _parametros.Add("Id", id, DbType.Int32);
+
+                return _ctx.SQLServerConexao.Query<UsuarioQueryResult>(UsuarioQueries.Obter, _parametros).FirstOrDefault();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
         }
 
         public List<UsuarioQueryResult> Listar()
         {
-            throw new NotImplementedException();
+            try
+            {
+                return _ctx.SQLServerConexao.Query<UsuarioQueryResult>(UsuarioQueries.Listar).ToList();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
         }
 
         public UsuarioQueryResult Logar(string login, string senha)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _parametros.Add("Login", login, DbType.String);
+                _parametros.Add("Senha", senha, DbType.String);
+
+                return _ctx.SQLServerConexao.Query<UsuarioQueryResult>(UsuarioQueries.Logar, _parametros).FirstOrDefault();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
         }
 
         public bool CheckLogin(string login)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _parametros.Add("Login", login, DbType.String);
+
+                string retono = _ctx.SQLServerConexao.Query<string>(UsuarioQueries.CheckLogin, _parametros).FirstOrDefault();
+
+                return retono != null ? true : false;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
         }
 
         public bool CheckId(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _parametros.Add("Id", id, DbType.Int32);
+
+                return _ctx.SQLServerConexao.Query<bool>(UsuarioQueries.CheckId, _parametros).FirstOrDefault();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
         }
 
         public int LocalizarMaxId()
         {
-            throw new NotImplementedException();
+            try
+            {
+                return _ctx.SQLServerConexao.Query<int>(UsuarioQueries.LocalizarMaxId).FirstOrDefault();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
         }
     }
 }
